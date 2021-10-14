72aafbd7367cdb4c49a7ea3526b8ea3d9dee136a
Stinger-22 <56128124+Stinger-22@users.noreply.github.com>
2021-10-14 21:41:25 +0300

    +Background

--- main.py
+++ main.py
@@ -38,8 +38,17 @@ class Tile(Sprite):
 def mainloop():
     while True:
         clock.tick(60)
+        events = pygame.event.get()
+        binds(events)
         window.update()
         mainloop()
+
+
+def binds(events):
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
 
 pygame.init()
@@ -48,7 +57,11 @@ clock = pygame.time.Clock()
 window = pygame.display
 #window_width = window.Info().current_w
 #window_height = window.Info().current_h
-screenSurface = window.set_mode(size=(1080, 768), depth=32)
+
+bg_test = pygame.image.load("Art/bg.png")
+
+screen_surface = window.set_mode(size=(1024, 768), depth=32)
+screen_surface.blit(bg_test, [0, 0])
 window.set_caption('Game')
 
 mainloop()
