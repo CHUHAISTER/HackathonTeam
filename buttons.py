5cfef89b83f8300282a36f7203351a7f090de23e
Stinger-22 <maxym.sobol@gmail.com>
2021-10-15 02:34:33 +0300

    Created button Start Created Empty Class Button

---
+++ buttons.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+
+
+class Button:
+    def __init__(self, name, coords):
+        # Here should use f-strings
+        # self.surface = pygame.image.load("Art/button_start.png")
+
+        # self.rect = self.surface.get_rect(topleft=coords)
+        pass
+
+
+# Here I'll draw all buttons in menu
+def draw_buttons():
+    pass
+
+
+# Create dict {button: (lambda) function}
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+# -*- coding: utf-8 -*-
+
 import os
 import pygame
+
+"""
+pygame.image.load() - returns Surface
+Surface() - It's like plot on which I place pictures
+Surface.get_rect() - returns Rect
+Rect(left, top, width, height) - Saves coords of Surface
+Surface.blit(surface, coords) = on_this.draw(this, coords) (coords=(x,y) or Rect) 
+
+if event.type == pygame.MOUSEMOTION: event return (pos)
+"""
 
 
 class Sprite:
@@ -40,6 +52,10 @@ def mainloop():
         clock.tick(60)
         events = pygame.event.get()
         binds(events)
+        if game_state == "Menu":
+            mouse_on_button(events)
+        elif game_state == "Playing":
+            pass
         window.update()
         mainloop()
 
@@ -51,18 +67,38 @@ def binds(events):
             exit()
 
 
+def mouse_on_button(events):
+    for event in events:
+        if event.type == 1025 and event.button == 1:
+            if button_start_rect[0] < event.pos[0] < button_start_rect[0] + button_start_rect[2] and \
+            button_start_rect[1] < event.pos[1] < button_start_rect[1] + button_start_rect[3]:
+                global game_state
+                game_state = "Playing"
+                screen_surface.blit(level_surface, (0, 0))
+
+
 pygame.init()
 clock = pygame.time.Clock()
 
 window = pygame.display
-#window_width = window.Info().current_w
-#window_height = window.Info().current_h
 
-bg_test = pygame.image.load("Art/bg.png")
+window_width = 1366
+window_height = 768
 
-screen_surface = window.set_mode(size=(1024, 768), depth=32)
-screen_surface.blit(bg_test, [0, 0])
+# Set screen_surface
+screen_surface = window.set_mode(size=(window_width, window_height), flags=pygame.FULLSCREEN, depth=32)
+screen_surface.fill((150, 30, 0))
 window.set_caption('Game')
 
-mainloop()
+# Set level_surface
+level_surface = pygame.Surface((window_width, window_height))
+level_surface.fill((255, 255, 255))
 
+# Start Button
+button_start_surface = pygame.image.load("Art/button_start.png")
+button_start_rect = button_start_surface.get_rect(topleft=(100, 100))
+screen_surface.blit(button_start_surface, button_start_rect)
+
+game_state = "Menu"
+
+mainloop()
